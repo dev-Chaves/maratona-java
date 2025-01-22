f55bd736ab5198d1d1c6a68b1f537a0c9d21c330
chaves <chavesprogrammer@gmail.com>
2025-01-22 12:52:24 -0300

    aula de get e set / private

--- src/academy/devdojo/maratonajava/PooCore/dominio/Pessoa.java
+++ src/academy/devdojo/maratonajava/PooCore/dominio/Pessoa.java
@@ -1,4 +1,32 @@
 package academy.devdojo.maratonajava.PooCore.dominio;
 
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private String cpf;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0){
+            System.out.println("Idade Inválida");
+            return;
+        }
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void imprimi(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+    }
 }
