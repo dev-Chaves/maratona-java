90bf1eb7d4abb0ed05809d38c06bcbb273d3e084
chaves <chavesprogrammer@gmail.com>
2025-02-10 10:57:29 -0300

    reduz cod/métodoCalculaImposto

--- src/academy/devdojo/maratonajava/polimorfismo/service/CalculaImposto.java
+++ src/academy/devdojo/maratonajava/polimorfismo/service/CalculaImposto.java
@@ -1,25 +1,24 @@
 package academy.devdojo.maratonajava.polimorfismo.service;
 
 import academy.devdojo.maratonajava.polimorfismo.domain.Computador;
+import academy.devdojo.maratonajava.polimorfismo.domain.Produto;
 import academy.devdojo.maratonajava.polimorfismo.domain.Tomate;
 
 public class CalculaImposto {
-    public static void calcularImpostoComputador(Computador computador){
-        System.out.println("Relatório de Imposto PC");
-        double imposto = computador.calcularImposto();
-        System.out.println("Computador " + computador.getNome());
-        System.out.println("Valor "+ computador.getValor());
-        System.out.println("Imposto a ser pago "+ imposto);
-        System.out.println();
+
+    public static void calcularImposto(Produto produto){
+
+        System.out.println("Relatório Imposto");
+
+        double imposto = produto.calcularImposto();
+
+        System.out.println("Produto: "+ produto.getNome());
+        System.out.println("Valor: "+ produto.getValor());
+        System.out.println("Imposto a ser pago: "+ imposto);
+
+        System.out.println("--------------------------------");
+
     }
 
-    public static void calcularImpostoTomate(Tomate tomate){
-        System.out.println("Relatório de Imposto Tomate");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Computador " + tomate.getNome());
-        System.out.println("Valor "+ tomate.getValor());
-        System.out.println("Imposto a ser pago "+ imposto);
-        System.out.println();
-    }
 
 }
