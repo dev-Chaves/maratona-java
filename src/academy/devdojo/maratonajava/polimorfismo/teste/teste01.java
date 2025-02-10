c9829d32c52223149a051807b2341e698bfde38e
chaves <chavesprogrammer@gmail.com>
2025-02-10 11:22:58 -0300

    testes tv

--- src/academy/devdojo/maratonajava/polimorfismo/teste/teste01.java
+++ src/academy/devdojo/maratonajava/polimorfismo/teste/teste01.java
@@ -2,13 +2,21 @@ package academy.devdojo.maratonajava.polimorfismo.teste;
 
 import academy.devdojo.maratonajava.polimorfismo.domain.Computador;
 import academy.devdojo.maratonajava.polimorfismo.domain.Produto;
+import academy.devdojo.maratonajava.polimorfismo.domain.Televisao;
+import academy.devdojo.maratonajava.polimorfismo.service.CalculaImposto;
 
 public class teste01 {
     public static void main(String[] args) {
         Produto produto = new Computador("RYZEN", 3000L);
 
-        System.out.println(produto.getNome());
-        System.out.println(produto.getValor());
+        Produto tv = new Televisao("Sony", 4000);
+
+//        System.out.println(produto.getNome());
+//        System.out.println(produto.getValor());
+
+//        System.out.println(tv.calcularImposto());
+
+        CalculaImposto.calcularImposto(tv);
 
     }
 }
