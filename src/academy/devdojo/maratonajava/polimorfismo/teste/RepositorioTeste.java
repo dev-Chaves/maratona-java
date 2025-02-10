3ee0578b5f004d8b8fe94357643e1767c7c12771
chaves <chavesprogrammer@gmail.com>
2025-02-10 14:07:19 -0300

    poliformismo/interface

--- src/academy/devdojo/maratonajava/polimorfismo/teste/RepositorioTeste.java
+++ src/academy/devdojo/maratonajava/polimorfismo/teste/RepositorioTeste.java
@@ -2,10 +2,11 @@ package academy.devdojo.maratonajava.polimorfismo.teste;
 
 import academy.devdojo.maratonajava.polimorfismo.repositorio.Repositorio;
 import academy.devdojo.maratonajava.polimorfismo.service.RepositorioBancoDeDados;
+import academy.devdojo.maratonajava.polimorfismo.service.RepositorioMemoria;
 
 public class RepositorioTeste {
     public static void main(String[] args) {
-        Repositorio repositorio = new RepositorioBancoDeDados();
+        Repositorio repositorio = new RepositorioMemoria();
 
         repositorio.salvar();
 
