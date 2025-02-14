3c78aefef2495457c3d88d2f2f91d52412ace27b
chaves <chavesprogrammer@gmail.com>
2025-02-14 16:56:31 -0300

    teste n finalizado

---
+++ src/academy/devdojo/maratonajava/String/teste/SpringTeste02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.String.teste;
+
+public class SpringTeste02 {
+    public static void main(String[] args) {
+
+        String chaves = "chaves";
+
+        System.out.println(chaves.charAt(5));
+
+        for (int i=0; i<chaves.length(); i++) {
+            if (c.equals("a")) {
+                System.out.println("Achamos A");
+            } else {
+                System.out.println("Passou");
+            }
+        }
+
+
+
+    }
+}
