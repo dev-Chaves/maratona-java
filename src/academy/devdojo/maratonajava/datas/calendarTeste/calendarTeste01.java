348052c1babdac78078b87c9b848e7bf9bac9ba8
chaves <chavesprogrammer@gmail.com>
2025-02-17 12:00:05 -0300

    formatação de datas/horarios

--- src/academy/devdojo/maratonajava/datas/calendarTeste/calendarTeste01.java
+++ src/academy/devdojo/maratonajava/datas/calendarTeste/calendarTeste01.java
@@ -1,4 +1,27 @@
 package academy.devdojo.maratonajava.datas.calendarTeste;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class calendarTeste01 {
+    public static void main(String[] args) {
+
+        // é um função abstrata e não permite o instanciamento
+        Calendar c = Calendar.getInstance();
+
+        Date date = c.getTime();
+
+        System.out.println(c);
+
+        System.out.println("---------------------------------------------------");
+
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+
+        System.out.println("---------------------------------------------------");
+
+        System.out.println(date);
+
+    }
 }
--- src/academy/devdojo/maratonajava/datas/dateFormat/dateFormatTest.java
+++ src/academy/devdojo/maratonajava/datas/dateFormat/dateFormatTest.java
@@ -1,4 +1,32 @@
 package academy.devdojo.maratonajava.datas.dateFormat;
 
+import javax.swing.text.DateFormatter;
+import java.text.DateFormat;
+import java.util.Calendar;
+
 public class dateFormatTest {
+    public static void main(String[] args) {
+
+        Calendar caledar = Calendar.getInstance();
+
+        DateFormat[] df = new DateFormat[7];
+
+        df[0] = DateFormat.getInstance();
+        df[1] = DateFormat.getDateInstance();
+        df[2] = DateFormat.getDateTimeInstance();
+        df[3] = DateFormat.getTimeInstance(DateFormat.SHORT);
+        df[4] = DateFormat.getTimeInstance(DateFormat.MEDIUM);
+        df[5] = DateFormat.getDateInstance(DateFormat.LONG);
+        df[6] = DateFormat.getDateInstance(DateFormat.FULL);
+
+
+        for (DateFormat date : df) {
+
+            System.out.println(date.format(caledar.getTime()));
+            System.out.println("______________________________________");
+
+        }
+
+
+    }
 }
