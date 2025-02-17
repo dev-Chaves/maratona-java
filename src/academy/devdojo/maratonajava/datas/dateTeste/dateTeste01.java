828fda862532a6564e58dbc5753dd059653da129
chaves <chavesprogrammer@gmail.com>
2025-02-17 11:59:24 -0300

    pegar data pelo Date(obsoleto)

--- src/academy/devdojo/maratonajava/datas/dateTeste/dateTeste01.java
+++ src/academy/devdojo/maratonajava/datas/dateTeste/dateTeste01.java
@@ -1,4 +1,14 @@
 package academy.devdojo.maratonajava.datas.dateTeste;
 
+import java.util.Date;
+
 public class dateTeste01 {
+    public static void main(String[] args) {
+
+        Date date = new Date(1_000_000_000L
+        );
+
+        System.out.println(date);
+
+    }
 }
