4e7b58306b590c77dff2e46a5152e2c802a366a2
chaves <chavesprogrammer@gmail.com>
2025-02-18 09:51:03 -0300

    equals obj

--- src/academy/devdojo/maratonajava/collections/equals/domain/Celular.java
+++ src/academy/devdojo/maratonajava/collections/equals/domain/Celular.java
@@ -1,4 +1,38 @@
 package academy.devdojo.maratonajava.collections.equals.domain;
 
 public class Celular {
+    String serie;
+    String nome;
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj==null) return false;
+        if (this == obj)  return false;
+        if (this.getClass() != obj.getClass()) return false;
+        Celular celular = (Celular) obj;
+        return serie != null && serie.equals(celular.serie);
+    }
+
+    public Celular(String serie, String nome) {
+        this.serie = serie;
+        this.nome = nome;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
+
+
--- src/academy/devdojo/maratonajava/collections/equals/teste/equalsTeste.java
+++ src/academy/devdojo/maratonajava/collections/equals/teste/equalsTeste.java
@@ -1,13 +1,21 @@
-package academy.devdojo.maratonajava.collections;
+package academy.devdojo.maratonajava.collections.equals.teste;
 
-public class equals {
+import academy.devdojo.maratonajava.collections.equals.domain.Celular;
+
+public class equalsTeste {
     public static void main(String[] args) {
-        
+
     String nome = "chaves";
     String name = new String("chaves");
 
-        System.out.println(nome.equals(name));
+    System.out.println(nome.equals(name));
 
+    Celular celular1 = new Celular("ABC1", "samsug");
+    Celular celular2 = celular1;
+
+    // Em objeto, ele se refere pelo THIS.
+
+    System.out.println(celular1.equals(celular2));
 
     }
 }
