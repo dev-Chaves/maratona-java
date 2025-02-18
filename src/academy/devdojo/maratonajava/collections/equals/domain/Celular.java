6e1ca27655be92ec6006ea0a4c50724146b220c6
chaves <chavesprogrammer@gmail.com>
2025-02-18 09:49:30 -0300

    equals em objs e strings

---
+++ src/academy/devdojo/maratonajava/collections/equals/domain/Celular.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava.collections.equals.domain;
+
+public class Celular {
+}
---
+++ src/academy/devdojo/maratonajava/collections/equals/teste/equalsTeste.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.collections;
+
+public class equals {
+    public static void main(String[] args) {
+        
+    String nome = "chaves";
+    String name = new String("chaves");
+
+        System.out.println(nome.equals(name));
+
+
+    }
+}
